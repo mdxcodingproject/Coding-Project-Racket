00b4c559d6d5ec32595958f87185e546e8750829
utreah <ersoyugur1528@gmail.com>
2024-11-27 23:19:09 +0000

    added get-account-password/id logic to parse from struct -u
    I couldn't access to the band and fan list even though I was only reading. So I created a func to return value from struct members

--- structs.rkt
+++ structs.rkt
@@ -1,8 +1,15 @@
 #lang racket/gui
 (struct band-user-struct (name surname ID password) #:mutable)
 (struct fan-user-struct (name surname ID password) #:mutable)
-(define band-list(list))
-(define fan-list(list))
+
+(define test-acc(band-user-struct "Ugur" "Ersoy" 123 123))
+(define test-acc2(band-user-struct "Murat" "Ersoy" 345 567))
+
+(define fan-test-acc (fan-user-struct "Alice" "Johnson" 6969 5252))
+(define fan-test-acc2 (fan-user-struct "Bob" "Smith" 3152 3131))
+
+(define band-list(list test-acc test-acc2))
+(define fan-list(list fan-test-acc fan-test-acc2))
 
 (define (create-account name surname id password radiobutton)
   (cond
@@ -11,10 +18,8 @@
        ((equal? radiobutton 0) (let ((new_account (band-user-struct name surname id password)))
                               (set! band-list (cons new_account band-list))))
        ((equal? radiobutton 1) (let ((new_account (fan-user-struct name surname id password)))
-                              (set! band-list (cons new_account fan-list))))))
+                              (set! fan-list (cons new_account fan-list))))))
     (else (message-box "Warning" "Invalid Argument(s)! All text fields must be filled!\n"))))
-
-
 
 (define (check-registration-status id radio-selection)
   (cond
@@ -25,6 +30,19 @@
      (for ([i fan-list])
        (equal? id (fan-user-struct-ID i)) #t))
     (else #f)))
+
+
+(define (get-account-password struct-name account-type)
+  (cond
+    ((equal? account-type 0) (band-user-struct-password struct-name))
+    ((equal? account-type 1) (fan-user-struct-password struct-name))
+    (else (displayln "Error Pass"))))
+
+(define (get-account-id struct-name account-type)
+  (cond
+    ((equal? account-type 0) (band-user-struct-ID struct-name))
+    ((equal? account-type 1) (fan-user-struct-ID struct-name))
+  (else (displayln "Error id"))))
 
 (provide (all-defined-out))
 
