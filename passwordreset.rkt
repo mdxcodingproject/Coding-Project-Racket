44be956949fd64f6d5253b2ba4f0ea2743db7e9c
utreah <mdxcodingproject@gmail.com>
2024-11-28 14:27:45 +0000

    Update on find-secret-answer function, password-reset-screen,

--- passwordreset.rkt
+++ passwordreset.rkt
@@ -1,39 +1,59 @@
 #lang racket/gui
 (require "registerwindow.rkt")
 (require "structs.rkt")
-(define (find-secret-answer user-secret acc-type)
-  (let ([flag 0])
-    (cond
-      ((equal? acc-type 0)
-       (for ([i band-list])
-         (cond
-           ((equal? user-secret (band-user-struct-secret-answer i)) (printf "SA: ~a\n" (band-user-struct-secret-answer i)) (set! flag 1))))))
-    (cond
-      ((equal? acc-type 1)
-       (for ([i fan-list])
-         (cond
-           ((equal? user-secret (fan-user-struct-secret-answer i)) (printf "SA: ~a\n" (fan-user-struct-secret-answer i)) (set! flag 1))))))
-    (cond
-      ((equal? flag 0) (display "no secret answer\n")))))
+(define password-reset-flag 0)
+
+(define (find-secret-answer user-id user-secret acc-type)
+  (cond
+    ((equal? acc-type 0)
+     (for ([i band-list])
+       (cond
+         ((and (equal? user-secret (band-user-struct-secret-answer i)) (equal? user-id (band-user-struct-ID i)))(set! password-reset-flag 1))))))
+  (cond
+    ((equal? acc-type 1)
+     (for ([i fan-list])
+       (cond
+         ((and (equal? user-secret (fan-user-struct-secret-answer i)) (equal? user-id (fan-user-struct-ID i))) (set! password-reset-flag 1))))))
+  (cond
+    ((equal? password-reset-flag 0) (message-box "Warning" "no secret answer\n"))))
 
 
-
-(define (password-reset)
+(define (password-reset-screen)
   (define password-reset-frame (new frame% [label "Password Reset"] [width 300] [height 200]))
-  (define passresset-radio-button (new radio-box% [parent password-reset-frame] [label ""] [choices (list "Band" "Fan")]))
+  (define passreset-radio-button (new radio-box% [parent password-reset-frame] [label ""] [choices (list "Band" "Fan")]))
+  (define user-id-preset-text-field (new text-field% [parent password-reset-frame] [label "User ID"]))
   (define secret-answer-text-field (new text-field% [parent password-reset-frame] [label "Secret Answer"]))
-  (define request-reset-button (new button% [parent password-reset-frame] [label "Request Reset"] [callback (lambda (button event)
-                                                                                                              (let ([user-secret-ans (send secret-answer-text-field get-value)]
-                                                                                                                    [acc-type (send passresset-radio-button get-selection)])
-                                                                                                                (printf "Ans: ~a Acc-type: ~a\n" user-secret-ans acc-type)
-                                                                                                                (find-secret-answer user-secret-ans acc-type)))]))
+  ; (define request-reset-button (new button% [parent password-reset-frame] [label "Request Reset"] [callback (lambda (button event)
+  ;                                                                                                            (let ([user-secret-ans (send secret-answer-text-field get-value)]
+  ;                                                                                                                   [acc-type (send passreset-radio-button get-selection)]
+  ;                                                                                                                   (user-id (string->number(send user-id-preset-text-field get-value))))
+  ;                                                                                                               (find-secret-answer user-id user-secret-ans acc-type)))]))
                                                                                                                    
-  
+
+  (define new-password-text-field (new text-field% [parent password-reset-frame] [label "New Password"]))
+  (define reset-button (new button% [parent password-reset-frame] [label "Submit Request"] [callback (lambda (button event)
+                                                                                                       (let ([new-pass (send new-password-text-field get-value)]
+                                                                                                             [user-secret-ans (send secret-answer-text-field get-value)]
+                                                                                                             (acc-type (send passreset-radio-button get-selection))
+                                                                                                             (user-id (string->number(send user-id-preset-text-field get-value))))
+                                                                                                         (printf "Reset Button> S.Ans: ~a Acc-type: ~a\n" user-secret-ans acc-type)
+                                                                                                         (find-secret-answer user-id user-secret-ans acc-type)
+                                                                                                         (cond
+                                                                                                           ((eq? password-reset-flag 1)
+                                                                                                            (set-account-password user-id new-pass acc-type)
+                                                                                                            (displayln (band))
+                                                                                                            (printf "Reset Button> New pass: ~a Acc-type: ~a" new-pass acc-type))
+                                                                                                           (else (message-box "Warning" (format "ID: ~a or Secret Answer: ~a wrong!" user-id user-secret-ans))))))]))
+                                                                                                          
 
 
+   (send user-id-preset-text-field set-value "123")
+  (send secret-answer-text-field set-value "oyku")
+  (send new-password-text-field set-value "706")
 
   
   (send password-reset-frame show #t))
-
+(define (band)
+  band-list)
 (provide (all-defined-out))
 ;
